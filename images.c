23868a78cab735815a5234649da9c90cb3db777c
Ramakanth <gopaganiramakanth@outlook.com>
2024-02-20 15:43:23 +0530

    updated images.c emergency

---
+++ images.c
@@ -0,0 +1,7 @@
+Now imagine that we are not yet done with hello-world-images, but we need to fix an error on master.
+
+I don't want to mess with master directly, and I do not want to mess with hello-world-images, since it is not done yet.
+
+We have made changes in this file, and we need to get those changes to the master branch.
+
+Check the status:
